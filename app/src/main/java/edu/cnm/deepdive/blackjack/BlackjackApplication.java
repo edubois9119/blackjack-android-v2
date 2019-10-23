a1e7b04a0a95dc20fad77e7ffeda0470eb59775d
edubois9119 <erica.dubois9119@gmail.com>
2019-10-23 10:22:38 -0600

    Implemented Google Sign In

--- app/src/main/java/edu/cnm/deepdive/blackjack/BlackjackApplication.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/BlackjackApplication.java
@@ -5,12 +5,14 @@ import android.widget.ImageView;
 import com.facebook.stetho.Stetho;
 import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.blackjack.service.BlackjackDatabase;
+import edu.cnm.deepdive.blackjack.service.GoogleSignInService;
 
 public class BlackjackApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    GoogleSignInService.setApplicationContext(this);
     Stetho.initializeWithDefaults(this);
     Picasso.setSingletonInstance(new Picasso.Builder(this).loggingEnabled(true).build());
     BlackjackDatabase.setApplicationContext(this);
